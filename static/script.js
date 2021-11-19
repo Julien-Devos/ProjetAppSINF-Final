2021d78f125b83b40b0e268cc90574e83975a352
Julien-Devos <julien.devos@student.uclouvain.be>
2021-11-19 12:43:36 +0100

    Home page finished

---
+++ static/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {
+
+    async function unfade(element) {
+        var op = 0.1;  // initial opacity
+        element.style.display = 'block';
+        var timer = setInterval(function () {
+            if (op >= 1) {
+                clearInterval(timer);
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += op * 0.1;
+        }, 10);
+    }
+
+    unfade(document.getElementById("nav"))
+    unfade(document.getElementById("content"))
+
+});
