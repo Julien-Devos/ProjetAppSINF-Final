48daba40ca8353104537962cd00b1e6541135e65
Julien-Devos <72694305+Julien-Devos@users.noreply.github.com>
2021-11-21 23:25:15 +0100

    Added dark theme

--- static/script.js
+++ static/script.js
@@ -16,22 +16,53 @@ window.addEventListener('load', function() {
 
     fade(document.getElementById("body"))
 
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
 
 
     // Apply dark theme or light theme when button is clicked
     let themeBtn = document.getElementById("theme-toggle");
     let themetxt = document.getElementById("theme-txt");
     let icon = document.getElementById("theme-icon");
+    let theme = getCookie("theme");
 
+    if (theme === "dark"){
+        document.body.classList.toggle("dark-theme");
+        themetxt.textContent = "Mode jour";
+        icon.classList.toggle("icon-Light-Mode");
+        icon.classList.remove("icon-Dark-Mode");
+    }
     if (themeBtn !== null){
         themeBtn.onclick = function (){
             document.body.classList.toggle("dark-theme");
             if(document.body.classList.contains("dark-theme")){
+                setCookie("theme","dark")
                 themetxt.textContent = "Mode jour";
                 icon.classList.toggle("icon-Light-Mode");
                 icon.classList.remove("icon-Dark-Mode");
             }
             else{
+                setCookie("theme","light")
                 themetxt.textContent = "Mode nuit";
                 icon.classList.toggle("icon-Dark-Mode");
                 icon.classList.remove("icon-Light-Mode");
