74e7b37e3ec5dbccdf46781cbbe62fe040108c97
Julien-Devos <72694305+Julien-Devos@users.noreply.github.com>
2021-11-21 03:25:53 +0100

    Added profile dropdown

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
 
-    async function unfade(element) {
+    // Make a smooth fade in when the page load
+    function fade(element) {
         var op = 0.1;  // initial opacity
         element.style.display = 'block';
         var timer = setInterval(function () {
@@ -13,7 +14,39 @@ window.addEventListener('load', function() {
         }, 10);
     }
 
-    unfade(document.getElementById("nav"))
-    unfade(document.getElementById("content"))
+    fade(document.getElementById("body"))
+
+
+
+    // Apply dark theme or light theme when button is clicked
+    let themeBtn = document.getElementById("theme-toggle");
+    let themetxt = document.getElementById("theme-txt");
+    let icon = document.getElementById("theme-icon");
+
+    if (themeBtn !== null){
+        themeBtn.onclick = function (){
+            document.body.classList.toggle("dark-theme");
+            if(document.body.classList.contains("dark-theme")){
+                themetxt.textContent = "Mode jour";
+                icon.classList.toggle("icon-Light-Mode");
+                icon.classList.remove("icon-Dark-Mode");
+            }
+            else{
+                themetxt.textContent = "Mode nuit";
+                icon.classList.toggle("icon-Dark-Mode");
+                icon.classList.remove("icon-Light-Mode");
+            }
+        }
+    }
+
+
+    // prevent leaving the dropdown-menu when clicking on it
+    $(function() {
+
+        $('.dropdown-menu').on('click', function(event) {
+            event.stopPropagation();
+        });
+
+    });
 
 });
